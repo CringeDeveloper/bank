aa1cb6dd27a89feb8541e28a94e94d6ca839183b
Vladislav <warzon131@gmail.com>
2024-12-14 21:25:03 +0200

    fix

--- db/query/entry.sql
+++ db/query/entry.sql
@@ -12,8 +12,9 @@ LIMIT 1;
 -- name: ListEntries :many
 SELECT *
 FROM entries
+WHERE account_id = $1
 ORDER BY id
-LIMIT $1 OFFSET $2;
+LIMIT $2 OFFSET $3;
 
 -- name: UpdateEntry :one
 UPDATE entries
