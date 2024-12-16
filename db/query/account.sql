832eec151d663610b21d09c4732d120842c90ffa
Vladislav <warzon131@gmail.com>
2024-12-17 00:03:45 +0200

    add

--- db/query/account.sql
+++ db/query/account.sql
@@ -13,8 +13,7 @@ LIMIT 1;
 SELECT *
 FROM accounts
 WHERE id = $1
-LIMIT 1
-FOR NO KEY UPDATE;
+LIMIT 1 FOR NO KEY UPDATE;
 
 -- name: ListAccounts :many
 SELECT *
@@ -26,6 +25,12 @@ LIMIT $1 OFFSET $2;
 UPDATE accounts
 SET balance = $2
 WHERE id = $1
+RETURNING *;
+
+-- name: AddAccountBalance :one
+UPDATE accounts
+SET balance = sqlc.arg(amount)
+WHERE id = sqlc.arg(id)
 RETURNING *;
 
 -- name: DeleteAccount :exec
