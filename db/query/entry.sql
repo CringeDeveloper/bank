6b308d786e220afea78389de16e7c9b1e03a38c1
Vladislav <warzon131@gmail.com>
2024-12-16 18:16:12 +0200

    update

--- db/query/entry.sql
+++ db/query/entry.sql
@@ -15,14 +15,3 @@ FROM entries
 WHERE account_id = $1
 ORDER BY id
 LIMIT $2 OFFSET $3;
-
--- name: UpdateEntry :one
-UPDATE entries
-SET amount = $2
-WHERE id = $1
-RETURNING *;
-
--- name: DeleteEntry :exec
-DELETE
-FROM entries
-WHERE id = $1;
--- db/query/transfer.sql
+++ db/query/transfer.sql
@@ -16,14 +16,3 @@ WHERE from_account_id = $1
    OR to_account_id = $2
 ORDER BY id
 LIMIT $3 OFFSET $4;
-
--- name: UpdateTransfer :one
-UPDATE transfers
-SET amount = $2
-WHERE id = $1
-RETURNING *;
-
--- name: DeleteTransfer :exec
-DELETE
-FROM transfers
-WHERE id = $1;
