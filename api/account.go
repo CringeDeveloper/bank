d3cc92e36d906650007fb26082988cd20871d0de
Vladislav <warzon131@gmail.com>
2024-12-21 21:37:59 +0200

    add account handler

---
+++ api/account.go
@@ -0,0 +1,37 @@
+package api
+
+import (
+	db "github.com/cringedeveloper/bank/db/sqlc"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+const defaultBalance = 0
+
+type createAccountRequest struct {
+	Owner    string `json:"owner" binding:"required"`
+	Currency string `json:"currency" binding:"required,oneof=USD EUR"`
+}
+
+func (server *Server) createAccount(ctx *gin.Context) {
+	var req createAccountRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.CreateAccountParams{
+		Owner:    req.Owner,
+		Currency: req.Currency,
+		Balance:  defaultBalance,
+	}
+
+	account, err := server.store.CreateAccount(ctx, arg)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
