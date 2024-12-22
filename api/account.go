b7aeec19348a7e9f9d3443bc67602b855e91ba86
Vladislav <warzon131@gmail.com>
2024-12-22 02:37:48 +0200

    add delete

--- api/account.go
+++ api/account.go
@@ -118,3 +118,27 @@ func (server *Server) updateAccount(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusOK, account)
 }
+
+type deleteAccountRequest struct {
+	ID int64 `json:"id" binding:"required"`
+}
+
+func (server *Server) deleteAccount(ctx *gin.Context) {
+	var req deleteAccountRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	err := server.store.DeleteAccount(ctx, req.ID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"message": "Account successfully deleted"})
+}
