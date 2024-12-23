c4f82370244b620e49a289f25b3322bd6504fb6f
Vladislav <warzon131@gmail.com>
2024-12-23 03:36:51 +0200

    install viper

--- main.go
+++ main.go
@@ -4,19 +4,19 @@ import (
 	"database/sql"
 	"github.com/cringedeveloper/bank/api"
 	db "github.com/cringedeveloper/bank/db/sqlc"
+	"github.com/cringedeveloper/bank/util"
 	"log"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "localhost:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	cfg, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot read config")
+	}
+
+	conn, err := sql.Open(cfg.DBDriver, cfg.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -24,7 +24,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(cfg.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
